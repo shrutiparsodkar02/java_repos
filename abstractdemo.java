7a6be7aab89d16100d56209b56fd1a9fc8eabf86
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-04-20 23:37:53 +0530

    adding code

---
+++ abstractdemo.java
@@ -0,0 +1,12 @@
+abstract class MYworld{
+	abstract void display();
+	void display3(){};
+}
+class Myclass extends MYworld{
+	void display3(){
+	
+	}
+	public static void main(String args[]){
+		
+	}
+}
